63ecd6bbe83d78c469b92f925b529c476643663e
Denis Belan <belan.denis@gmail.com>
2021-08-21 18:41:28 +0300

    Fix with hidden RegisterConditions method

--- source/Control.cs
+++ source/Control.cs
@@ -75,7 +75,8 @@ namespace DynamicShops
                 Logger.LogError(e);
             }
         }
-        private static void RegisterConditions(Assembly assembly)
+
+        public static void RegisterConditions(Assembly assembly)
         {
             foreach (var type in assembly.GetTypes())
             {
