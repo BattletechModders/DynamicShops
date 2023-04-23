6572cdd36268183a3acbc9d054eb1a97ebe80554
Denis Belan <belan.denis@gmail.com>
2023-04-23 17:21:45 +0300

    Update DCustomShop.cs

--- source/Shops/DCustomShop.cs
+++ source/Shops/DCustomShop.cs
@@ -7,93 +7,91 @@ using System.Linq;
 using UnityEngine;
 using c = CustomShops.Control;
 
-namespace DynamicShops.Shops
+namespace DynamicShops.Shops;
+
+public class DCustomShop : TaggedShop, ITextIcon, IDefaultPrice, IDiscountFromFaction
 {
-    public class DCustomShop : TaggedShop, ITextIcon, IDefaultPrice, IDiscountFromFaction
+    public DCustomShopDescriptor Descriptor { get; private set; }
+
+    public override bool CanUse => UseConditions == null ||
+                                   UseConditions.All(i => i.IfApply(c.State.Sim, c.State.CurrentSystem));
+    public override bool Exists => ExistsConditions == null ||
+                                   ExistsConditions.All(i => i.IfApply(c.State.Sim, c.State.CurrentSystem));
+
+    public override int SortOrder => Descriptor.SortOrder;
+    public override bool RefreshOnSystemChange => true;
+    public override bool RefreshOnMonthChange => true;
+    public override bool RefreshOnOwnerChange => false;
+
+    protected override void UpdateTags()
     {
-        public DCustomShopDescriptor Descriptor { get; private set; }
+        List<string> tags = new List<string>();
 
-        public override bool CanUse => UseConditions == null ||
-                                       UseConditions.All(i => i.IfApply(c.State.Sim, c.State.CurrentSystem));
-        public override bool Exists => ExistsConditions == null ||
-                                       ExistsConditions.All(i => i.IfApply(c.State.Sim, c.State.CurrentSystem));
+        // Skip if no shops of given type exists
+        if (!Control.CustomShopDefs.ContainsKey(Name))
+            return;
 
-        public override int SortOrder => Descriptor.SortOrder;
-        public override bool RefreshOnSystemChange => true;
-        public override bool RefreshOnMonthChange => true;
-        public override bool RefreshOnOwnerChange => false;
+        // Get list of shopdefs with given custom name
+        List<DCustomShopDef> defs = Control.CustomShopDefs[Name];
 
-        protected override void UpdateTags()
+        foreach (var shop_def in defs)
         {
-            List<string> tags = new List<string>();
+            Control.LogDebug(DInfo.Conditions, $"Start to check conditions:");
 
-            // Skip if no shops of given type exists
-            if (!Control.CustomShopDefs.ContainsKey(Name))
-                return;
-
-            // Get list of shopdefs with given custom name
-            List<DCustomShopDef> defs = Control.CustomShopDefs[Name];
-
-            foreach (var shop_def in defs)
+            var use = true;
+            if (shop_def.Conditions != null)
             {
-                Control.LogDebug(DInfo.Conditions, $"Start to check conditions:");
-
-                var use = true;
-                if (shop_def.Conditions != null)
+                foreach (var cond in shop_def.Conditions)
                 {
-                    foreach (var cond in shop_def.Conditions)
+                    Control.LogDebug(DInfo.Conditions, $"- {cond.GetType()}");
+                    if (!cond.IfApply(c.State.Sim, c.State.CurrentSystem))
                     {
-                        Control.LogDebug(DInfo.Conditions, $"- {cond.GetType()}");
-                        if (!cond.IfApply(c.State.Sim, c.State.CurrentSystem))
-                        {
-                            use = false;
-                            break;
-                        }
+                        use = false;
+                        break;
                     }
                 }
-                else
-                {
-                    Control.LogDebug(DInfo.Conditions, $"- empty");
-                }
-                if (use)
-                {
-                    Control.LogDebug(DInfo.Conditions, DebugTools.ShowList("passed", shop_def.Items));
-                    tags.AddRange(shop_def.Items);
-                }
-            }           
-            Tags = tags;
-        }
-
-        public override string Name => Descriptor.Name;
-        public override string TabText => Descriptor.TabText;
-
-        public override string ShopPanelImage => SG_Stores_StoreImagePanel.STORE_ILLUSTRATION;
-
-        public override Color IconColor => Color.white;
-        public override Color ShopColor => LazySingletonBehavior<UIManager>.Instance.UILookAndColorConstants.SystemStoreColor.color;
-
-        public List<DCondition> UseConditions => Descriptor.UseConditions;
-        public List<DCondition> ExistsConditions => Descriptor.ExistConditions;
-
-        public virtual string SpriteID
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(Descriptor.Icon))
-                {
-                    return "uixSvgIcon_ammoBox_Ballistic";
-                }
-
-                return Descriptor.Icon;
             }
-        }
+            else
+            {
+                Control.LogDebug(DInfo.Conditions, $"- empty");
+            }
+            if (use)
+            {
+                Control.LogDebug(DInfo.Conditions, DebugTools.ShowList("passed", shop_def.Items));
+                tags.AddRange(shop_def.Items);
+            }
+        }           
+        Tags = tags;
+    }
 
-        public FactionValue RelatedFaction => c.State.CurrentSystem.OwnerValue;
+    public override string Name => Descriptor.Name;
+    public override string TabText => Descriptor.TabText;
 
-        public DCustomShop(DCustomShopDescriptor decriptor)
+    public override string ShopPanelImage => SG_Stores_StoreImagePanel.STORE_ILLUSTRATION;
+
+    public override Color IconColor => Color.white;
+    public override Color ShopColor => LazySingletonBehavior<UIManager>.Instance.UILookAndColorConstants.SystemStoreColor.color;
+
+    public List<DCondition> UseConditions => Descriptor.UseConditions;
+    public List<DCondition> ExistsConditions => Descriptor.ExistConditions;
+
+    public virtual string SpriteID
+    {
+        get
         {
-            Descriptor = decriptor;
+            if (string.IsNullOrEmpty(Descriptor.Icon))
+            {
+                return "uixSvgIcon_ammoBox_Ballistic";
+            }
+
+            return Descriptor.Icon;
         }
     }
 
+    public FactionValue RelatedFaction => c.State.CurrentSystem.OwnerValue;
+
+    public DCustomShop(DCustomShopDescriptor decriptor)
+    {
+        Descriptor = decriptor;
+    }
 }
